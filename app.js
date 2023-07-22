acdc4cd5d095d238c97a1fc9d2e49a14571456b6
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-07-22 23:36:01 +0200

    ADDED: env-file

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
-const express = require('express')
-const logger = require('morgan') 
-const cors = require('cors')
-const fs = require('fs/promises');
-const mongoose = require('mongoose');
-require('dotenv').config('./.env');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const fs = require("fs/promises");
 
+const noticesRouter = require("./routes/api/notices");
+const authRouter = require("./routes/api/authRoutes");
+const petsRouter = require("./routes/api/pets");
+const app = express();
 
-mongoose.connect(process.env.MONGO_PASS).then(() => {
-  console.log("Database connection successful");
-}).catch(() => {
-  console.log("NO Database connection")
-    process.exit(1);
-})
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const noticesRouter = require('./routes/api/notices')
-const authRouter = require('./routes/api/authRoutes')
-const petsRouter = require('./routes/api/pets')
-const app = express()
+app.use(logger(formatsLogger));
+app.use(cors());
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-app.use(express.static('public'))
-app.use('/api/notices', noticesRouter)
-app.use('/api/users', authRouter)
-app.use('/api/pets', petsRouter)
+app.use("/api/notices", noticesRouter);
+app.use("/api/users", authRouter);
+app.use("/api/pets", petsRouter);
 
-
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'found' })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "found" });
+});
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+
+const app = require("./app");
+require("dotenv").config("./.env");
+
+mongoose
+  .connect(process.env.MONGO_PASS)
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch(() => {
+    console.log("NO Database connection");
+    process.exit(1);
+  });
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
