a7403e10d80a954cfae30aa104d4cdb05e76c38c
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-07-23 13:43:04 +0200

    FIXED: conflict

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -8,13 +8,13 @@ router.post('/register', signup);
 router.post('/login', login);
 router.post('/logout', authenticate, logout);
 router.post('/current', authenticate, getCurrent);
-<<<<<<< Updated upstream
+
 router.patch('/:id', authenticate, addFavorites);
 router.delete('/:id', authenticate, removeFavorites);
-=======
+
 router.patch('/favorites/:id', authenticate, addFavorites);
 router.delete('/favorites/:id', authenticate, removeFavorites);
->>>>>>> Stashed changes
+
 router.get('/favorites', authenticate, getFavorites);
 router.patch('/', authenticate, changeUserData);
 router.patch('/avatar', authenticate, upload.single("avatar"), changeAvatarImg);
