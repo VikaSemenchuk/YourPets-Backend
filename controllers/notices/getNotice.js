1d4d24b5d1f09b150e34899da70a54d64b0c7818
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-07-25 21:21:59 +0200

    FIXED: notice logic

--- controllers/notices/getNotice.js
+++ controllers/notices/getNotice.js
@@ -1,21 +1,19 @@
-const fs = require('fs/promises');
-const Notice = require('../../models/notices/notices');
+const fs = require("fs/promises");
+const Notice = require("../../models/notices/notices");
 
 const getNoticeById = async (req, res) => {
-
   try {
     const { id } = req.params;
     const item = await Notice.findById(id);
     if (!item) {
-      return res.status(404).json({ "message": "Not found" })
+      return res.status(404).json({ message: "Not found" });
     }
     return res.status(200).json(item);
-    } catch (err) {
-    res.status(400).json({ message: 'Ooops...'})
-    }
-}
-
+  } catch (err) {
+    res.status(400).json({ message: "Ooops...", stack: err.stack });
+  }
+};
 
 module.exports = {
-    getNoticeById
-}
+  getNoticeById,
+};
--- routes/api/notices.js
+++ routes/api/notices.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const {filterNotices, listNotices, getNoticeById, removeNotice, addNotice, updateImgNotice, getAllNotices, upload} = require('../../controllers/index')
 const authenticate = require('../../middlewares/authMiddleware');
+const {filterNotices,  getNoticeById, removeNotice, addNotice, updateImgNotice, getAllNotices, listNotices, upload} = require('../../controllers/index')
 
-router.get('/users', authenticate, listNotices)
 router.get('/', getAllNotices)
+router.get('/users', authenticate, listNotices)
 
 router.get('/search', filterNotices)
+
 
 router.get('/:id', getNoticeById)
 
 router.post('/', authenticate, addNotice)
 
-router.delete('/users/:id', authenticate, removeNotice) // add /owner
+router.delete('/users/:id', authenticate, removeNotice) 
 
 
 router.patch('/avatar/:id', authenticate, upload.single("img"), updateImgNotice)
