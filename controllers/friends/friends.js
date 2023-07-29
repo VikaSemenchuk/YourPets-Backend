dae3b700531e4f28b3ea78bba3a34caaf61967a6
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-07-29 20:45:13 +0200

    CONNECTED: friends with DB

--- controllers/friends/friends.js
+++ controllers/friends/friends.js
@@ -1,9 +1,20 @@
+const { checkResult } = require("../../helpers");
 const friends = require("../../models/friends/friends");
+const Friend = require("../../models/friends/friends")
 
 const getAllFriends = async (req, res) => {
-    const result = await friends.listFriends();
-    console.log('result :>> ', result);
-    res.json(result);
+   try {
+    const getAllList = await Friend.find()
+    console.log('getAllFriends :>> ', getAllList);
+
+    checkResult(getAllList)
+    return res.status(200).json(getAllList)
+   } catch (error) {
+    res.status(500).json(error.message)
+   }
+    // const result = await friends.listFriends();
+    // console.log('result :>> ', result);
+    // res.json(result);
   };
 
   module.exports = {
--- models/friends/friends.js
+++ models/friends/friends.js
@@ -15,3 +15,25 @@ const listFriends = async () => {
   };
 
   module.exports = {listFriends}
+
+  const mongoose = require('mongoose');
+  const Schema = mongoose.Schema;
+
+  const friendsSchema = mongoose.Schema({
+     title: String,
+    url: String,
+    addressUrl: String,
+    imageUrl: String,
+    address: String,
+    workDays: Array,
+    phone: String,
+    email: String
+  },
+  {
+    timestamps: true,
+    versionKey: false
+})
+
+const Friend = mongoose.model('Friend', friendsSchema);
+
+module.exports = Friend
