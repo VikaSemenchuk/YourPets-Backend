6f5ba6bc095f97800f913309132647dfd3902be3
Vika Semenchuk <vikasemenchuk@MacBook-Air-Vika.local>
2023-07-31 16:10:25 +0200

    FIXED: notice response

--- controllers/notices/getAllNotices.js
+++ controllers/notices/getAllNotices.js
@@ -2,14 +2,22 @@ const { Notice } = require("../../models/notices");
 
 const getAllNotices = async (req, res) => {
   try {
-    const { page = 1, limit = 4 } = req.query;
-    const skip = (page - 1) * limit;
+    const { page = 1 } = req.query;
+    // const skip = (page - 1) * limit;
 
-    const listAllNotices = await Notice.find({}, "-createdAT -updatedAT", {
-      skip,
-      limit,
-    }).sort({ createdAt: -1 });
+    // const { page = 1, limit = 4 } = req.query;
+    // const skip = (page - 1) * limit;
 
+    const listAllNotices = await Notice.find(
+      {},
+      "-createdAT -updatedAT"
+      , {
+       page
+      // limit,
+      // skip
+      }
+    ).sort({ createdAt: -1 });
+    
     return res.status(200).json(listAllNotices);
   } catch (err) {
     console.log(err);
