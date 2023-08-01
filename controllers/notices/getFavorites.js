0070042f9544b58b22338367e668f08bd836436b
Vika Semenchuk <vikasemenchuk@MacBook-Air-Vika.local>
2023-08-01 13:54:24 +0200

    FIXED: fav notices

--- controllers/notices/getFavorites.js
+++ controllers/notices/getFavorites.js
@@ -1,13 +1,19 @@
 const User = require("../../models/users/users");
 
 const getFavorites = async (req, res) => {
-    const { id } = req.user;
+    const { _id } = req.user;
     try {
-      const newUser = await User.findById(req.user._id);
+      const newUser = await User.findById(_id);
+
+      const usersFavNotices = newUser.favorites
   
+      const total = usersFavNotices.length
+      console.log('total :>> ', total);
+
+      
       // console.log('newUser.favorites :>> ', newUser.favorites);
   
-      return res.status(200).json(newUser.favorites);
+      return res.status(200).json({usersFavNotices, total});
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: "Ooops... ListContacts" });
