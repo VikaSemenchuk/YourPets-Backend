56cb871a42fbcaaf7c3491d9d80b174c8dcb6538
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-08-02 05:02:11 +0200

    FIXED: news search

--- controllers/news/newsSearch.js
+++ controllers/news/newsSearch.js
@@ -1,19 +1,21 @@
 const { New } = require("../../models/news");
-const { checkTitle } = require("../../helpers/checkTitle");
+const { checkTitle, checkTitle2 } = require("../../helpers/checkTitle");
 
 const newsSearch = async (req, res) => {
   try {
     const { title } = req.query;
-
     if (!title) return res.status(400).json({ message: "Bed request" });
 
-    let newsList = [];
+    const { page = 1, limit = 9 } = req.query;
+    const skip = (page - 1) * limit;
 
-    newsList = await New.find({}, "-createdAT -updatedAT").sort({ date: -1 });
+    const allNotices = await New.find({});
 
-    newsList = checkTitle(newsList, title);
+    const newsList = checkTitle2(allNotices, title, skip, limit).noticesSlice;
 
-    return res.status(200).json(newsList);
+    const total = checkTitle2(allNotices, title, skip, limit).total;
+
+    return res.status(200).json({ newsList, total });
   } catch (error) {
     res.status(500).json({ message: "Ooops... news, " });
   }
