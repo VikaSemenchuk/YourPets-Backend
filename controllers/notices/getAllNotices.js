a44974d4be78b94eba826d482ffef6593a631fd2
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2023-10-08 23:07:21 +0200

    ADDED: pagination logic in getAllNotices

--- controllers/notices/getAllNotices.js
+++ controllers/notices/getAllNotices.js
@@ -1,19 +1,24 @@
 const { Notice } = require("../../models/notices");
 const { checkResult } = require("../../helpers");
+const { now } = require("mongoose");
 
 const getAllNotices = async (req, res, next) => {
   try {
     const { title, category, sex, date } = req.query;
+    let {page = 1, limit = 8} = req.query
+
+    page = +page
+    limit = +limit
+    const skip = (page - 1) * limit;
 
     const pipeline = [];
     const $match = {};
 
-    let newList;
-  let startDate;
-  let endDate;
-  let startDate2;
-  let endDate2;
-  const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+    let startDate;
+    let endDate;
+    let startDate2;
+    let endDate2;
+    const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
 
     if (category) {
       $match["category"] = category;
@@ -32,33 +37,175 @@ const getAllNotices = async (req, res, next) => {
     }
 
     if (date) {
-      switch (date) {
-        case "1":
-    // const rightDate = date.split("-").reverse().join("-");
+      // const ageFilter = 1; // Значення, яке приходить з фронтенда (1, 2, 3 або 4)
 
-          diff = new Date() - millisecondsInYear
-          startDate = new Date(diff)
-          // console.log('startDate :>> ', startDate);
-          endDate = new Date()
-          // console.log('endDate :>> ', endDate);
-      // return {startDate, endDate}
+      // let startDate, endDate;
 
-          break;
-        case "2":
+      // Визначення відповідних дат для фільтрації
+      if (date === "1") {
+        // Фільтруємо тварин, які молодші за 1 рік (менше одного року)
+        endDate = new Date();
+        startDate = new Date();
+        startDate.setFullYear(endDate.getFullYear() - 1);
+      } else if (date === "2") {
+        // Фільтруємо тварин, які молодші за 2 роки (менше одного року)
+        endDate = new Date();
+        endDate.setFullYear(endDate.getFullYear() - 1);
 
-        default:
-          break;
+        startDate = new Date();
+        startDate.setFullYear(endDate.getFullYear() - 2);
+      } else if (date === "3") {
+        // Фільтруємо тварин, які старші за 2 роки (більше 2 років)
+        endDate = new Date();
+        endDate.setFullYear(endDate.getFullYear() - 2);
+      } else if (date === "4") {
+        // Фільтруємо тварин, які молодші за 1 рік і старші за 2 роки
+        endDate = new Date();
+        endDate.setFullYear(endDate.getFullYear() - 2);
+        startDate = new Date();
+        startDate.setFullYear(endDate.getFullYear() - 1);
       }
-      // return {startDate, endDate}
-      $match["date"] = {
-        $gte: startDate,
-        $lte: endDate,
-      };
-      console.log('startDate :>> ', startDate);
-    }
-    console.log('endDate :>> ', endDate);
 
-    pipeline.push({ $match });
+      // Підготовка фільтру для $match
+      // const filter = {
+      //   date: {},
+      // };
+
+      // if (startDate) {
+      //   filter.date.$gte = startDate;
+      // }
+
+      // if (endDate) {
+      //   filter.date.$lt = endDate;
+      // }
+
+      // const pipeline = [
+      //   {
+      // $match["date"] = filter;
+
+      $match["date"] = {
+               $gte: startDate,
+              $lte: endDate,
+            };
+
+      // Виконуємо агрегаційний запит MongoDB з використанням пайплайну
+      // db.collection.aggregate(pipeline);
+
+      // switch (date) {
+      //   case "1":
+      //     // const rightDate = date.split("-").reverse().join("-");
+
+      //     diff = new Date() - millisecondsInYear;
+      //     startDate = new Date(diff);
+      //     console.log("startDate :>> ", startDate);
+      //     endDate = new Date();
+      //     console.log("endDate :>> ", endDate);
+      //     // return {startDate, endDate}
+
+      //     $match["date"] = {
+      //       $gte: startDate,
+      //       $lte: endDate,
+      //     };
+      //     break;
+      //   case "2":
+      //     diff = new Date() - millisecondsInYear;
+
+      //     // diff2years = new Date() - millisecondsInYear * 2;
+      //     // startDate = new Date(diff2years);
+      //     // console.log("startDate2 :>> ", startDate);
+
+      //     // diff = startDate + millisecondsInYear
+      //     endDate = new Date(diff);
+
+      //     console.log("endDate2 :>> ", endDate);
+
+      //     $match["date"] = {
+      //       // $gt: startDate,
+      //       $lt: endDate,
+      //     };
+      //     break;
+      //   case "3":
+      //     diff = new Date().getTime() - millisecondsInYear * 2;
+
+      //     endDate = new Date(diff);
+
+      //     console.log('new Date().getTime() :>> ', new Date().getTime());
+
+      //     console.log("endDate3 :>> ", endDate);
+
+      //     $match["date"] = {
+      //       // $gt: startDate,
+      //       $lt: endDate,
+      //     };
+      //     break;
+      //   case "4":
+
+      //     diffTo1Year = new Date().getTime() - millisecondsInYear;
+      //     console.log('diffTo1Year :>> ', diffTo1Year);
+      //     startDate = new Date(diffTo1Year);
+      //     console.log("startDate :>> ", startDate);
+      //     endDate = new Date();
+      //     console.log("endDate :>> ", endDate);
+      //     // return {startDate, endDate}
+
+      //     diffUp2Years = new Date() - millisecondsInYear * 2;
+
+      //     // console.log('diffUp2Years :>> ', diffUp2Years);
+      //     endDate2 = new Date(diffUp2Years);
+      //     console.log('endDate2 :>> ', endDate2);
+
+      //     $match: {
+
+      //         $or: [
+      //           {
+      //             date: {
+      //               $gt: startDate,
+      //               $lt: endDate
+      //             }
+      //           },
+      //           {
+      //             date: {
+      //               // $gte: startDate2,
+      //               $gt: endDate2
+      //             }
+      //           }
+      //         ]
+
+      //     }
+
+      //     // $match["$or"] = [
+      //     //   {
+      //     //     date: {
+      //     //       $gte: startDate,
+      //     //       $lte: endDate,
+      //     //     },
+      //     //     date: {
+      //     //       $lt: endDate2,
+      //     //     },
+      //     //   },
+      //     // ];
+
+      //     break;
+
+      //   default:
+      //     endDate = new Date();
+      //     console.log("endDate5 :>> ", endDate);
+      // }
+      // return {startDate, endDate}
+    }
+
+    const total = await Notice.countDocuments($match);
+  if (!total) return { total, noticesList: [] };
+
+
+    // pipeline.push({ $match });
+
+    pipeline.push(
+      {$match},
+      { $skip: skip },
+      { $limit: limit },
+      { $sort: { createdAt: -1 } }
+    );
 
     const noticesList = await Notice.aggregate(pipeline);
 
@@ -76,7 +223,7 @@ const getAllNotices = async (req, res, next) => {
     // checkResult(noticesList)
     // checkResult(total)
 
-    return res.status(200).json({ noticesList /*, total*/ });
+    return res.status(200).json({ noticesList, total });
   } catch (err) {
     next(err);
   }
